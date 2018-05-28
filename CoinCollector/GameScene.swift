73c3dc2c6c47c735b61457498448b370e5724a91
ngt-kn <nagatak@Dev.local>
2018-05-28 05:58:07 -0700

    add gameover and restart functionality

--- CoinCollector/GameScene.swift
+++ CoinCollector/GameScene.swift
@@ -4,7 +4,15 @@
 //
 //  Created by Kenneth Nagata on 5/28/18.
 //  Copyright © 2018 Kenneth Nagata. All rights reserved.
-// <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
+//  Credit for assets
+//  <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//  <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+//  <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
+
+
+
 
 import SpriteKit
 import GameplayKit
@@ -14,10 +22,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var coinMan : SKSpriteNode?
     var spawnCoinTimer : Timer?
+    var spawnBombTimer : Timer?
     var ground: SKSpriteNode?
     var ceil: SKSpriteNode?
     var scoreLabel: SKLabelNode?
     var score: Int = 0
+    var gameOverScoreLabel : SKLabelNode?
+    var finalScoreLabel : SKLabelNode?
     
     let coinManCategory : UInt32 = 0x1 << 1
     let coinCategory : UInt32 = 0x1 << 2
@@ -43,16 +54,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
         
-        spawnCoinTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
-            self.createCoin()
-        })
-        
+        startTimers()
     }
     
-    
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        coinMan?.physicsBody?.applyForce(CGVector(dx: 0, dy: 100_000))
+        
+        if scene?.isPaused == false {
+            coinMan?.physicsBody?.applyForce(CGVector(dx: 0, dy: 50_000))
+        }
+        
+        
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+            let theNodes = nodes(at: location)
+            for node in theNodes {
+                if node.name == "play" {
+                    score = 0
+                    node.removeFromParent()
+                    finalScoreLabel?.removeFromParent()
+                    gameOverScoreLabel?.removeFromParent()
+                    scene?.isPaused = false
+                    scoreLabel?.text = "Score: \(score)"
+                    startTimers()
+                }
+            }
+        }
     }
     
     func createCoin() {
@@ -76,16 +102,92 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
+    func createBomb() {
+        let bomb = SKSpriteNode(imageNamed: "bomb")
+        bomb.physicsBody = SKPhysicsBody(rectangleOf: bomb.size)
+        bomb.physicsBody?.affectedByGravity = false
+        bomb.physicsBody?.categoryBitMask = bombCategory
+        bomb.physicsBody?.contactTestBitMask = coinManCategory
+        bomb.physicsBody?.collisionBitMask = 0
+        
+        addChild(bomb)
+        
+        let moveLeft = SKAction.moveBy(x: -size.width - bomb.size.width, y: 0, duration: 4)
+        let maxY = size.height / 2 - bomb.size.height / 2
+        let minY = -size.height / 2 + bomb.size.height / 2
+        let range = maxY - minY
+        let bombY = maxY - CGFloat(arc4random_uniform(UInt32(range)))
+        
+        bomb.position = CGPoint(x: self.size.width / 2 + bomb.size.width / 2, y: bombY)
+        bomb.run(SKAction.sequence([moveLeft, SKAction.removeFromParent()]))
+        
+    }
+    
     func didBegin(_ contact: SKPhysicsContact) {
-        score += 1
-        scoreLabel?.text = "Score: \(score)"
+
         
         if contact.bodyA.categoryBitMask == coinCategory {
             contact.bodyA.node?.removeFromParent()
+            addScore()
         }
         
         if contact.bodyB.categoryBitMask == coinCategory {
             contact.bodyB.node?.removeFromParent()
+            addScore()
+        }
+        
+        if contact.bodyA.categoryBitMask == bombCategory {
+            contact.bodyA.node?.removeFromParent()
+            gameOver()
+        }
+        
+        if contact.bodyB.categoryBitMask == bombCategory {
+            contact.bodyB.node?.removeFromParent()
+            gameOver()
         }
     }
+    
+    func startTimers(){
+        spawnCoinTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.createCoin()
+        })
+        
+        spawnBombTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (timer) in
+            self.createBomb()
+        })
+    }
+    
+    func addScore(){
+        score += 1
+        scoreLabel?.text = "Score: \(score)"
+    }
+    
+    func gameOver() {
+        scene?.isPaused = true
+        spawnCoinTimer?.invalidate()
+        spawnBombTimer?.invalidate()
+     
+        gameOverScoreLabel = SKLabelNode(text: "Your Score:")
+        gameOverScoreLabel?.position = CGPoint(x: 0, y: 200)
+        gameOverScoreLabel?.fontSize = 100
+        gameOverScoreLabel?.zPosition = 1
+        if gameOverScoreLabel != nil {
+            addChild(gameOverScoreLabel!)
+        }
+        
+        finalScoreLabel = SKLabelNode(text: "\(score)")
+        finalScoreLabel?.position = CGPoint(x: 0, y: 0)
+        finalScoreLabel?.fontSize = 150
+        finalScoreLabel?.zPosition = 1
+        if finalScoreLabel != nil {
+            addChild(finalScoreLabel!)
+        }
+        
+        let playButton = SKSpriteNode(imageNamed: "play")
+        playButton.position = CGPoint(x: 0, y: -200)
+        playButton.name = "play"
+        playButton.zPosition = 1
+        addChild(playButton)
+    }
+    
 }
